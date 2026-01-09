ad8f28a56863a14df699d4ff8df2fe82a7db8bf3
caramelpopcornKHU <hyungmin8528@naver.com>
2026-01-09 15:52:46 +0900

    [Silver V] Title: 영화감독 숌, Time: 136 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -4,13 +4,19 @@ import java.io.*;
 public class Main {
 
 	public int solution(int n) {
-		
+
 		int cnt = 0;
+
 		for (int i = 666; i <= 2666799; i++) {
-			if(String.valueOf(i).contains("666")) {
-				cnt++;
+			int temp = i;
+			while (temp > 0) {
+				if (temp % 1000 == 666) {
+					cnt++;
+					break;
+				}
+				temp /= 10;
 			}
-			if(cnt==n)
+			if(cnt == n)
 				return i;
 		}
 
