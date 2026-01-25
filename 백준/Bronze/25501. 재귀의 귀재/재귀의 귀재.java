1cb79d9559fadda0d1e50d07809b953834ad5ef1
caramelpopcornKHU <hyungmin8528@naver.com>
2026-01-25 18:28:40 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 268 ms, Memory: 20148 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int count;
+
+	public static int recursion(String s, int l, int r, int cnt) {
+		count = cnt;
+		if (l >= r) {
+			return 1;
+		} else if (s.charAt(l) != s.charAt(r)) {
+			return 0;
+		} else {
+			return recursion(s, l + 1, r - 1, cnt + 1);
+		}
+	}
+
+	public static int isPalindrome(String s) {
+		return recursion(s, 0, s.length() - 1, 1);
+	}
+
+	public static void main(String[] args) throws IOException {
+		Main T = new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		String[] arr = new String[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = br.readLine();
+		}
+
+		for (String x : arr) {
+			System.out.println(isPalindrome(x) + " " + count);
+		}
+
+//		System.out.println("ABBA: " + isPalindrome("ABBA"));
+//		System.out.println("ABC: " + isPalindrome("ABC"));
+
+	}
+}
