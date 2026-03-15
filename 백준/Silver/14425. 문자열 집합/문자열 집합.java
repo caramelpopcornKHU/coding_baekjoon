b2a1bd014f2658493307e2bd40a09442bc3b11ac
caramelpopcornKHU <hyungmin8528@naver.com>
2026-03-16 08:48:27 +0900

    [Silver IV] Title: 문자열 집합, Time: 424 ms, Memory: 32472 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -4,18 +4,13 @@ import java.util.*;
 
 public class Main {
 	
-	static int[] ch;
 	
-	public int solution(int n, int m, String[] nArr, String[] mArr) {
+	public int solution(int n, int m, Set<String> nSet, String[] mArr) {
 		int answer = 0;
 		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if(nArr[i].equals(mArr[j])) {
-//					System.out.println(mArr[j]);
-					answer++;
-					
-				}
+		for(String x : mArr) {
+			if(nSet.contains(x)) {
+				answer++;
 			}
 		}
 		
@@ -31,14 +26,14 @@ public class Main {
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		
-		String[] nArr = new String[n];
+		Set<String> nSet = new TreeSet<>();
+		
 		String[] mArr = new String[m];
-		ch = new int[m];
 		
 		
 		for(int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			nArr[i] = st.nextToken();
+			nSet.add(st.nextToken());
 		}
 		
 		for(int i = 0; i < m; i++) {
@@ -46,7 +41,7 @@ public class Main {
 			mArr[i] = st.nextToken();
 		}
 		
-		System.out.println(T.solution(n,m,nArr,mArr));
+		System.out.println(T.solution(n,m,nSet,mArr));
 		
 		
 	}
